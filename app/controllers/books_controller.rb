1c36b35550584d3c37c8ef1861222c0ec682d236
EC2 Default User <ec2-user@ip-172-31-10-205.ap-northeast-1.compute.internal>
2023-02-01 14:11:43 +0000

    [Update]Task-03

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,12 @@
 class BooksController < ApplicationController
+
+  before_action :authenticate_user!
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: 'Book was successfully created.'
     else
       @books = Book.all
       render 'index'
@@ -17,9 +20,34 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+        redirect_to book_path(@book), notice: 'You have updated book successfully.'
+    else
+        render "edit"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def index
     @book = Book.new
     @users = User.all
@@ -13,6 +16,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+        render "edit"
+    else
+        redirect_to user_path(current_user)
+    end
   end
 
   def update
